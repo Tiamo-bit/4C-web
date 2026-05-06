abcef6ea10f108d826913164fa251d54f29cc1d3
sangchenbo <2425396237@qq.com>
2026-05-06 10:28:35 +0800

    Fix Cloudflare Pages asset base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/4C-web/' : '/',
+  base: process.env.CF_PAGES ? '/' : command === 'build' ? '/4C-web/' : '/',
 }))
